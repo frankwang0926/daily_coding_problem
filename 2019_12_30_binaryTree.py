e667691a3fd6f9f33f5277f3aa14d68e23e39fda
Biao Wang <frankwb@vpn-128-97-245-98.host.ucla.edu>
2019-12-30 23:00:58 +0800

    12.30

---
+++ 2019_12_30_binaryTree.py
@@ -0,0 +1,57 @@
+'''
+This problem was asked by Google.
+
+Given the root to a binary tree, 
+implement serialize(root), which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree.
+'''
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def serialize(root):
+    if root == None:
+        return "Null"
+    return "{} {} {}".format(root.val, serialize(root.left), serialize(root.right))
+
+def helper(list):
+    if list[0] == "Null":
+        list.pop(0)
+        return None
+    node = Node(list[0])
+    list.pop(0)
+    node.left = helper(list)
+    node.right = helper(list)
+    return node
+
+def deserialize(s):
+    list = s.split()
+    return helper(list)
+
+'''
+def serialize(root):
+    if root is None:
+        return '#'
+    return '{} {} {}'.format(root.val, serialize(root.left), serialize(root.right))
+
+def deserialize(data):
+    def helper():
+        val = next(vals)
+        if val == '#':
+            return None
+        node = Node(val)
+        node.left = helper()
+        node.right = helper()
+        return node
+    vals = iter(data.split())
+    return helper()
+'''
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+print(serialize(node))
+print(serialize(deserialize(serialize(node))))
+print(deserialize(serialize(node)).left.left.val == 'left.left')
